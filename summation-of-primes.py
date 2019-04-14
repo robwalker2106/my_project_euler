8531c90ee89f41f022f079d2ba912e92e77ba79c
Robert Walker <blkdragon2106@gmail.com>
2019-04-14 13:34:17 -0400

    Update summation-of-primes.py
    Faster, still show, but answered correctly.

--- summation-of-primes.py
+++ summation-of-primes.py
@@ -3,7 +3,6 @@
 
 
 primes = [2]
-c = 2
 
 
 
@@ -12,23 +11,23 @@ def current(num):
     for p in primes:
         if num % p == 0:
             return False
+    #print("not in prime")
     return True
         
 
-for i in range(3,2000001): 
-    while i > c:
-        if current(i):
-            c = primes[-1] + 1
-        if i == c:
-            primes.append(i)
-            print(primes[-1])
-            break
-        c = primes[-1] + 1
-
-        
-
-        
-    
-
+for i in range(3,2000001):
+    c = primes[-1]
+    stop = current(i)
+    if stop and i > c:
+        while i > c:
+            if i % c == 0:
+                break
+            else:
+                c = c + 1
+    if i == c:
+        primes.append(i)
+    print(primes[-1])
 
 print(sum(primes))
+
+
