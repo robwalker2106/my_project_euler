2f7b544002594946b7647c4c5ad9ca1d0fda567e
Robert Walker <blkdragon2106@gmail.com>
2019-04-16 21:45:45 -0400

    Create longest-collatz-sequence.py

---
+++ longest-collatz-sequence.py
@@ -0,0 +1,8 @@
+#Collatz Sequence
+#
+#if n is even, then n/2
+#if n is odd, the 3n + 1
+#
+# example for 13
+# 13 => 40 => 20 => 10 => 5 => 16 => 8 => 4 => 2 => 1
+#Which starting number under 1,000,000 produces the longest collatz sequence?
